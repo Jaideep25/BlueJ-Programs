47dcd85d666e193c68a6e11e51b418d924b67c0e
Jaideep <jaideepch@outlook.com>
2020-12-23 21:01:21 +0530

    Initial sharing of project

---
+++ AmstrongNumber.java
@@ -0,0 +1,22 @@
+class AmstrongNumber
+{
+    public static void main(int n)
+    {
+        int x;
+        x = n;
+        int D, sum = 0;
+        while(n>0)
+        {
+            D = n%10; 
+            System.out.println(D);
+            sum = sum+D*D*D;
+            System.out.println(sum);
+            n = n/10;
+            System.out.println(n);
+        }
+        if(sum ==x)
+            System.out.println("The number is an Amstrong number");
+        else
+            System.out.println("The number is not an amstrong number");
+    }
+}
--- NeonNumber.java
+++ NeonNumber.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class NeonNumber
 {
-    public static void main(String args[])
+    public static void main()
     {
         Scanner ob=new Scanner(System.in);
         System.out.println("Enter the number to be checked.");
