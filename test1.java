fa9dcd24c7801c6551cf48eaaaf31028a1c9cce1
Jaideep <jaideepch@outlook.com>
2020-12-24 13:26:37 +0530

    Initial sharing of project

---
+++ test1.java
@@ -0,0 +1,12 @@
+class test1
+{
+    public static void main(String args[])
+    {
+        int i;
+        for(i=19901;i<=20000;i++)
+        {
+            System.out.println(i);
+        }
+        
+    }
+}
