871c7f56f64db25e3b66a9d5be7fd32274b518c9
Jaideep <jaideepch@outlook.com>
2021-01-04 16:41:09 +0530

    Inital sharing of project

---
+++ RecordPrg3.java
@@ -0,0 +1,13 @@
+class RecordPrg3
+{
+    public static void main(String args[])
+    {
+        System.out.println("Multiple print statements part A");
+        System.out.println("$\t$\t$\t$");
+        System.out.println("\t$\t$\t$");
+        System.out.println("\t\t$\t$");
+        System.out.println("\t\t\t$\n\n");
+        System.out.println("Single print statement part B");
+        System.out.print("\t\t\t1\n\t\t2\t\t3\n\t4\t\t5\t\t6\n7\t\t8\t\t9\t\t10");
+    }
+}
