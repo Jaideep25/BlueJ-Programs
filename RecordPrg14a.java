2bded4fa90d6090a8cdde39e307489c4362adad4
Jaideep <jaideepch@outlook.com>
2021-01-04 12:43:20 +0530

    Inital sharing of project

--- RecordPrg14a.java
+++ RecordPrg14a.java
@@ -2,10 +2,10 @@ class RecordPrg14a
 {
     public static void main(String args[])
     {
-        int n,sum,sum1=0,i,j;
+        int n = 0,sum,sum1=0,i,j;
 
         System.out.println("\nEnter value for n = ");
-        System.out.println("%d",n);
+        //System.out.println("%d",n);
  
         for(i=1;i<=n;i++)
         {
@@ -16,6 +16,6 @@ class RecordPrg14a
                 }
                 sum1=sum1+sum;
         }
-        System.out.println("\nThe Sum of Series up to Value [ %d ] = [ %d ]\n",n,sum1);
+        //System.out.println("\nThe Sum of Series up to Value [ %d ] = [ %d ]\n",n,sum1);
     }
 }
