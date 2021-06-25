409fd795cd110ef969f3a8d8576d3e8bf84ceaeb
Jaideep25 <jaideepch@outlook.com>
2021-06-25 12:06:34 +0530

    Update

--- Paper2009.java
+++ Paper2009.java
@@ -1,11 +1,11 @@
 public class Paper2009
 {
-   public void num_calc(int num, char ch)
-   {
-       if(ch=='s')
-       {
-           int square=num*num;
-           System.out.println("The square of the number is = "+ square);
+    public void num_calc(int num, char ch)
+    {
+        if(ch=='s')
+        {
+            int square=num*num;
+            System.out.println("The square of the number is = "+ square);
         }
         else
         {
@@ -13,12 +13,13 @@ public class Paper2009
             System.out.println("The cube of the number is =  "+ cube);
         }
     }
+
     public void num_calc(int a, int b, char ch)
-   {
-       if(ch=='p')
-       {
-           int product=a*b;
-           System.out.println("The product of the two numbers is = "+ product);
+    {
+        if(ch=='p')
+        {
+            int product=a*b;
+            System.out.println("The product of the two numbers is = "+ product);
         }
         else
         {
@@ -26,15 +27,16 @@ public class Paper2009
             System.out.println("The sum of the two number is = "+ sum);
         }
     }
+
     public void num_calc(String s1, String s2)
-   {
-       if(s1.equals(s2))
-       {
-           System.out.println("The two strings are equal");
+    {
+        if(s1.equals(s2))
+        {
+            System.out.println("The two strings are equal");
         }
         else
         {
-          System.out.println("The two strings are not equal"); 
+            System.out.println("The two strings are not equal"); 
         }
     }
 }
